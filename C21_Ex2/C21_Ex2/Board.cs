f27fc3140499e4333b0f96ca3a5ea601b7dc599e
Yehonatan Bruchim <yonbruchim@gmail.com>
2021-08-20 13:58:30 +0300

    first commit

---
+++ C21_Ex2/C21_Ex2/Board.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Linq;
+
+namespace C21_Ex2
+{
+    public class Board
+    {
+        public Board(int boardSize)
+        {
+			boardGrid = new string[(int)Math.Pow(boardSize, 2)];
+			BoardGridSize = boardSize;
+		}
+
+		public void SetCell(int index, string player)
+		{
+			boardGrid[index] = player;
+		}
+
+		public string GetCell(int row, int col)
+        {
+			return boardGrid[row * BoardGridSize + col];
+		}
+
+		public bool IsBoardColumnValid(int col)
+        {
+			return (col >= 0 && col < BoardGridSize);
+        }
+
+		public void DrawBoard()
+        {
+			var numRows = (int)Math.Sqrt(boardGrid.Length);
+
+			Console.WriteLine();
+
+			for (int row = 0; row < numRows; row++)
+			{
+				if (row != 0)
+					Console.WriteLine(" " + string.Join("|", Enumerable.Repeat("---", numRows)));
+
+				Console.Write(" " + string.Join("|", Enumerable.Repeat("   ", numRows)) + "\n ");
+
+				for (int col = 0; col < numRows; col++)
+				{
+					if (col != 0)
+						Console.Write("|");
+					var space = boardGrid[row * numRows + col] ?? " ";
+					if (space.Length > 1)
+						Console.ForegroundColor = ConsoleColor.DarkGreen;
+					Console.Write(" " + space[0] + " ");
+					Console.ResetColor();
+				}
+
+				Console.WriteLine("\n " + string.Join("|", Enumerable.Repeat("   ", numRows)));
+			}
+
+			Console.WriteLine();
+		}
+
+		public bool IsBoardFull()
+        {
+			return boardGrid.All(space => space != null);
+		}
+
+		public string GetWinner()
+        {
+			var numRows = (int)Math.Sqrt(boardGrid.Length);
+
+			// Check rows
+			for (int row = 0; row < numRows; row++)
+			{
+				if (boardGrid[row * numRows] != null)
+				{
+					bool hasTicTacToe = true;
+					for (int col = 1; col < numRows && hasTicTacToe; col++)
+					{
+						if (boardGrid[row * numRows + col] != boardGrid[row * numRows])
+							hasTicTacToe = false;
+					}
+					if (hasTicTacToe)
+					{
+						// Put an indicator in the board to know which ones are part of the tic tac toe
+						for (int col = 0; col < numRows; col++)
+							boardGrid[row * numRows + col] += "!";
+						return boardGrid[row * numRows];
+					}
+				}
+			}
+
+			// Check columns
+			for (int col = 0; col < numRows; col++)
+			{
+				if (boardGrid[col] != null)
+				{
+					bool hasTicTacToe = true;
+					for (int row = 1; row < numRows && hasTicTacToe; row++)
+					{
+						if (boardGrid[row * numRows + col] != boardGrid[col])
+							hasTicTacToe = false;
+					}
+					if (hasTicTacToe)
+					{
+						// Put an indicator in the board to know which ones are part of the tic tac toe
+						for (int row = 0; row < numRows; row++)
+							boardGrid[row * numRows + col] += "!";
+						return boardGrid[col];
+					}
+				}
+			}
+
+			// Check top left -> bottom right diagonal
+			if (boardGrid[0] != null)
+			{
+				bool hasTicTacToe = true;
+				for (int row = 1; row < numRows && hasTicTacToe; row++)
+				{
+					if (boardGrid[row * numRows + row] != boardGrid[0])
+						hasTicTacToe = false;
+				}
+				if (hasTicTacToe)
+				{
+					// Put an indicator in the board to know which ones are part of the tic tac toe
+					for (int row = 0; row < numRows; row++)
+						boardGrid[row * numRows + row] += "!";
+					return boardGrid[0];
+				}
+			}
+
+			// Check top right -> bottom left diagonal
+			if (boardGrid[numRows - 1] != null)
+			{
+				bool hasTicTacToe = true;
+				for (int row = 1; row < numRows && hasTicTacToe; row++)
+				{
+					if (boardGrid[row * numRows + (numRows - 1 - row)] != boardGrid[numRows - 1])
+						hasTicTacToe = false;
+				}
+				if (hasTicTacToe)
+				{
+					// Put an indicator in the board to know which ones are part of the tic tac toe
+					for (int row = 0; row < numRows; row++)
+						boardGrid[row * numRows + (numRows - 1 - row)] += "!";
+					return boardGrid[numRows - 1];
+				}
+			}
+
+			return null;
+		}
+
+        private string[] boardGrid;
+        public int BoardGridSize { get; set; }
+
+    }
+}
---
+++ C21_Ex2/C21_Ex2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C21_Ex2
+{
+    class MainClass
+    {
+		static void Main(string[] args)
+		{
+			var stillPlaying = true;
+
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine("-----------------------");
+			Console.WriteLine("Welcome to Tic Tac Toe!");
+			Console.WriteLine("-----------------------\n");
+			Console.ResetColor();
+
+			while (stillPlaying)
+			{
+				Console.WriteLine("What would you like to do:");
+				Console.WriteLine("1. Start a new game");
+				Console.WriteLine("2. Quit\n");
+
+				Console.Write("Type a number and hit <enter>: ");
+
+				var choice = GetUserInput("[12]");
+
+				switch (choice)
+				{
+					case "1":
+						PlayGame();
+						Console.Clear();
+						break;
+					case "2":
+						stillPlaying = false;
+						break;
+				}
+			}
+		}
+
+		private static string GetUserInput(string validPattern = null)
+		{
+			var input = Console.ReadLine();
+			input = input.Trim();
+
+			if (validPattern != null && !System.Text.RegularExpressions.Regex.IsMatch(input, validPattern))
+			{
+				Console.ForegroundColor = ConsoleColor.DarkRed;
+				Console.WriteLine("\"" + input + "\" is not valid.\n");
+				Console.ResetColor();
+				return null;
+			}
+
+			return input;
+		}
+
+		private static void PlayGame()
+		{
+			string numRowsChoice = null;
+			while (numRowsChoice == null)
+			{
+				Console.Write("How many rows do you want to have? (3, 4, or 5) ");
+				numRowsChoice = GetUserInput("[345]");
+			}
+			var boardSize = int.Parse(numRowsChoice);
+			Board boardgrid = new Board(boardSize);
+
+			var turn = "X";
+			while (true)
+			{
+				Console.Clear();
+
+				var winner = boardgrid.GetWinner();
+				if (winner != null)
+				{
+					AnnounceResult(winner[0] + " WINS!!!");
+					boardgrid.DrawBoard();
+					break;
+				}
+				if (boardgrid.IsBoardFull())
+				{
+					AnnounceResult("It's a tie!!!");
+					boardgrid.DrawBoard();
+					break;
+				}
+
+				Console.WriteLine("Place your " + turn + ":");
+
+				boardgrid.DrawBoard();
+
+				var xoLoc = handleNextTurn(boardgrid);
+				boardgrid.SetCell(xoLoc, turn);
+
+				turn = turn == "X" ? "O" : "X";
+			}
+		}
+
+		private static void AnnounceResult(string message)
+		{
+			Console.WriteLine();
+
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine(message);
+			Console.ResetColor();
+
+			Console.Write("\nPress any key to continue...");
+			Console.CursorVisible = false;
+			Console.ReadKey();
+			Console.CursorVisible = true;
+		}
+
+		private static int handleNextTurn(Board boardgrid)
+		{
+			while (true)
+            {
+				Console.WriteLine("\nEnter a column and press <enter>");
+				var inputColStr = Console.ReadLine();
+				int inputCol = int.Parse(inputColStr);
+
+				if (!boardgrid.IsBoardColumnValid(inputCol))
+                {
+					Console.WriteLine("\nColumn out of range");
+				}
+
+				for (int row = boardgrid.BoardGridSize - 1; row >= 0 ; row--)
+				{
+					if (boardgrid.GetCell(row, inputCol) == null)
+                    {
+						return row * boardgrid.BoardGridSize + inputCol;
+					}
+
+				}
+				Console.WriteLine("\nColumn full");
+			}
+		}
+	}
+}
