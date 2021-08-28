4996f76f1e9a4785e81af203bfeff31ffb4efc56
Jonathan Bruchim <50236903+JonBruchim@users.noreply.github.com>
2021-08-28 13:00:29 +0300

    Update ValueOutOfRangeException.cs

--- Ex03/Ex03/ValueOutOfRangeException.cs
+++ Ex03/Ex03/ValueOutOfRangeException.cs
@@ -5,15 +5,14 @@ namespace Ex03
 {
     public class ValueOutOfRangeException : Exception
     {
-        float MaxValue;
-        float MinValue;
+        private readonly float r_MaxVal;
+        private readonly float r_MinVal;
 
-        public ValueOutOfRangeException()
-        {
-        }
-
-        public ValueOutOfRangeException(string message, float minValue, float maxValue) : base(message)
-        {
+        public ValueOutOfRangeException(float i_MaxVal, float i_MinVal)
+           : base(string.Format("Error:Value must be between {0} to {1}", i_MinVal, i_MaxVal))
+        { 
+            r_MaxVal = i_MaxVal;
+            r_MinVal = i_MinVal;
         }
     }
 }
